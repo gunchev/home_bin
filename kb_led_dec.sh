0fe8795d35e7e5b2e30f23eeeb1f8928e3860c7a
Doncho N. Gunchev <dgunchev@gmail.com>
2013-11-20 02:34:52 +0200

    Added kb_led_inc.sh and kb_led_dec.sh (I use because KDE increments by 3/10=0)

---
+++ kb_led_dec.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cur=$(dbus-send --print-reply=literal --type=method_call --system --dest=org.freedesktop.UPower /org/freedesktop/UPower/KbdBacklight org.freedesktop.UPower.KbdBacklight.GetBrightness | sed s/...int32.//g)
+max=$(dbus-send --print-reply=literal --type=method_call --system --dest=org.freedesktop.UPower /org/freedesktop/UPower/KbdBacklight org.freedesktop.UPower.KbdBacklight.GetMaxBrightness | sed s/...int32.//g)
+
+val=$(($cur - 1))
+if (($val >= 0)); then
+    dbus-send --print-reply=literal --type=method_call --system --dest=org.freedesktop.UPower /org/freedesktop/UPower/KbdBacklight org.freedesktop.UPower.KbdBacklight.SetBrightness int32:$val
+    notify-send -t 500 -i /usr/share/icons/HighContrast/scalable/devices/input-keyboard.svg "Brightness $val/$max"
+fi
